c80906c2d44b8ba363485b8f99d650efbae119ba
tanyakleynos@mail.ru <tanyakleynos@mail.ru>
2021-04-20 15:03:31 +0700

    Добавлены модели Добавлены модели для основных сущностей диаграммы классов (словарь, слово, вложение)

---
+++ LearnLatin/Data/Migrations/20210420080108_AddWordAttachments.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LearnLatin.Data.Migrations
+{
+    public partial class AddWordAttachments : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "WordAttachments",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    LatinWordId = table.Column<Guid>(nullable: false),
+                    Created = table.Column<DateTime>(nullable: false),
+                    Path = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WordAttachments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_WordAttachments_LatinWords_LatinWordId",
+                        column: x => x.LatinWordId,
+                        principalTable: "LatinWords",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WordAttachments_LatinWordId",
+                table: "WordAttachments",
+                column: "LatinWordId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WordAttachments");
+        }
+    }
+}
---
+++ LearnLatin/Models/WordAttachment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace LearnLatin.Models
+{
+    public class WordAttachment
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public Guid LatinWordId { get; set; }
+
+        public LatinWord LatinWord { get; set; }
+
+        public DateTime Created { get; set; }
+
+        [Required]
+        public String Path { get; set; }
+    }
+}
