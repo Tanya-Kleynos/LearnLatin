dc1a213e19b8ab8f51d96eeb1292a131a808fce2
Tanya-Kleynos <Tanya-Kleynos>
2021-06-29 09:48:02 +0700

    Немного изменены темы (пуш перед пуллом)

---
+++ LearnLatin/Models/EditViewModel/ThemeEditViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace LearnLatin.Models.EditViewModel
+{
+    public class ThemeEditViewModel
+    {
+        [Required]
+        public String Name { get; set; }
+        [Required]
+        public String Description { get; set; }
+    }
+}
